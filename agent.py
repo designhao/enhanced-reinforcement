ae6de4fe200079a1d9b4dcdcc02ba04286bd1e53
designhao <133928764+designhao@users.noreply.github.com>
2022-03-10 10:40:21 +0000

    doc: update German documentation

--- agent.py
+++ agent.py
@@ -42,4 +42,4 @@ class Agent:
                 else:
                     self.prev_utility_table = copy.deepcopy(self.utility_table)
 
-                logging.getLogger().debug("Episode %d: convergence %f", 
+                logging.getLogger().debug("Episode %d: convergence %f", executions, self.co
