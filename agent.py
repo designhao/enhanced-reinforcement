e963fb30e0244aa8017fcda27ea466a1eece2b2c
designhao <133928764+designhao@users.noreply.github.com>
2022-03-18 18:54:09 +0000

    Remove outdated tests

--- agent.py
+++ agent.py
@@ -51,4 +51,4 @@ class Agent:
         Return True if the change between previous util table and current util table
         are smaller than the convergence_threshold.
         """
-        s
+        self.convergence
