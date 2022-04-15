a36d99be9a2d7c5fd8077b9aeb4d979964a206ba
designhao <133928764+designhao@users.noreply.github.com>
2022-04-15 07:03:42 +0000

    Tidy up readme

--- common.py
+++ common.py
@@ -5,3 +5,4 @@ import os, sys, time, inspect, logging
 # Constants
 PATH        = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 DEFAULT_MAP = "maps/easy.txt"
+HIGH_PROB   = 0.
