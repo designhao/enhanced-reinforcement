81c11abd21642d9574e82acb8d11cc57fa67159b
designhao <133928764+designhao@users.noreply.github.com>
2022-04-19 05:33:02 +0000

    Temporally save

--- common.py
+++ common.py
@@ -8,4 +8,6 @@ DEFAULT_MAP = "maps/easy.txt"
 HIGH_PROB   = 0.7
 LOW_PROB    = 0.15
 DEBUG       = lambda: logging.getLogger().level == logging.DEBUG
-IS_DEBUG    = D
+IS_DEBUG    = DEBUG()
+
+MAX_TRAINING_E
