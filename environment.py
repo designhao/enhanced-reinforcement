8c50e3eb4cc806802f22e5d3faabddb9b9ff1c91
designhao <133928764+designhao@users.noreply.github.com>
2022-07-28 21:01:12 +0000

    Added:

--- environment.py
+++ environment.py
@@ -99,4 +99,4 @@ class Environment:
         available_actions = []
         succ_list = successors(state, self.map_data, self.map_width, self.map_height)
         for succ_state in succ_list:
-            action =
+            action = direction(stat
