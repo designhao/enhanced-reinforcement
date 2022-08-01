ed6fc897c6f14c356ac561e04677fffe43bb4812
designhao <133928764+designhao@users.noreply.github.com>
2022-08-01 11:19:03 +0000

    ren

--- environment.py
+++ environment.py
@@ -101,4 +101,6 @@ class Environment:
         for succ_state in succ_list:
             action = direction(state[0], state[1], succ_state[0], succ_state[1])
             available_actions.append(action)
-        return available_a
+        return available_actions
+
+    def ter
