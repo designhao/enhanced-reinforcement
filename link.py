820a122996347063249d20f296cc1f4bfc9f8970
designhao <133928764+designhao@users.noreply.github.com>
2022-09-07 15:19:28 +0000

    add an example

--- link.py
+++ link.py
@@ -4,3 +4,25 @@
 import math, copy, random, logging
 # import qvalue
 # from qvalue import *
+from common import *
+from util import *
+from agent import *
+
+class Link(Agent):
+
+    def __init__(self):
+
+        Agent.__init__(self)
+        self.q_values = dict()
+        self.frequency = dict()
+        self.prev_qtable = dict()
+        self.p_state = self.p_action = self.p_reward = self.state = self.action = self.reward = None
+        self.gamma = 0.9
+        self.env = None
+
+    
+    ################-------------------###################
+
+    def train(self, env):
+        """
+        Execute MAX_TRAINING_EPISODES rounds or until converge.
